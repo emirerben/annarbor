68329c5f2e00a66db73ce4a5983cc06824d239bc
erben <erben@umich.edu>
2022-10-19 18:44:03 -0400

    trying

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "56bdb86e4e409e8a"; }
+/******/ 		__webpack_require__.h = function() { return "f8e674640732ec7d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
